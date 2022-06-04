587aba2eeaf1a5045bc61fa72b0a061c2ba013ea
Roy Matero <roymatero@gmail.com>
2022-06-04 18:32:11 -0500

    Add anybar patch

--- config.h
+++ config.h
@@ -9,9 +9,11 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 20;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "JetBrains Mono:pixelsize=16" };
 static const char dmenufont[]       = "JetBrains Mono:pixelsize=16";
-// static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
